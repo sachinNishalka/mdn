4277b3784dcc4d54e6c4a001606c709b2ae824bc
sachinNishalka <72740598+sachinNishalka@users.noreply.github.com>
2022-11-03 10:30:49 +0530

    oop-inheritance

--- oop/oop-inheritance.js
+++ oop/oop-inheritance.js
@@ -2,3 +2,40 @@
 // a school has people
 // teachers and students
 // both teachers and students have common and different characteristics
+
+// class professor
+//     properties
+//         name
+//         teaches
+//     constuctor
+//         Professor(name, teaches)
+//     methods
+//         grade(paper)
+//         introduceself()
+// class student
+//     properties
+//         name
+//         year
+//     constuctor
+//         Student(name, year)
+//     methods
+//
+//         introduceself()
+
+// using the inheritance
+class person
+    properties 
+        name
+    constructor
+        Person(name)
+    methods
+        introduceself()
+class Professor : extends Person
+//     properties
+//   
+//         teaches
+//     constuctor
+//         Professor(name,teaches)
+//     methods
+//         grade(paper)
+//         
